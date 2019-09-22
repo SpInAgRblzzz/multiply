26d91903c34745cea3f34a04c006dd88623de81a
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-09-22 23:22:00 +0300

    Task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  debugger
+  let positionContainer = [];
+  for(let firstIndex = String(first).length - 1; firstIndex >= 0; firstIndex--){
+    for(let secondIndex = String(second).length - 1; secondIndex >= 0; secondIndex--){
+      const mult = String(first)[firstIndex] * String(second)[secondIndex];
+      positionContainer[firstIndex + secondIndex] = positionContainer[firstIndex + secondIndex] ? positionContainer[firstIndex + secondIndex] + mult : mult;      
+    }
+  }
+  positionContainer = positionContainer.reverse();
+  debugger
+  positionContainer.forEach(function(item, index){
+    const itemStr = String(item);
+    if(itemStr.length > 1){
+      positionContainer[index+1] = positionContainer[index+1] ? positionContainer[index+1]+ Number(itemStr.slice(0,itemStr.length-1)) : Number(itemStr.slice(0,itemStr.length-1));
+      positionContainer[index] = itemStr[itemStr.length - 1];
+    }
+  });
+
+  return positionContainer.reverse().join('');
 }
